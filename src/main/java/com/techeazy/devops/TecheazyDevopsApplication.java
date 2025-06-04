d0b5aea5642e20c5782a30fc6edac83872e47251
siddhesh <siddpawar583@gmail.com>
2025-06-04 13:17:04 +0530

    initial project stepup

---
+++ src/main/java/com/techeazy/devops/TecheazyDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.techeazy.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheazyDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheazyDevopsApplication.class, args);
+	}
+
+}
