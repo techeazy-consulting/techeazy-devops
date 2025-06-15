238f139979dbae2f27023f3026acf79c94b8739f
sumit-patil-24 <155406262+sumit-patil-24@users.noreply.github.com>
2025-06-15 17:18:00 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "sumit-4254"        #"your-terraform-state-bucket"
     key            = "terraform/terraform.tfstate"
-    region         = "us-west-1"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
